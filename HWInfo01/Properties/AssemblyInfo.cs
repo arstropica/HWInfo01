040bf2c5a0a4214889936ad00e7b6e44719d8ca6
ArsTropica <aowilliams@arstropica.com>
2015-02-06 17:05:25 -0500

    Initial Commit

---
+++ HWInfo01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HWInfo01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tropical Designs")]
+[assembly: AssemblyProduct("HWInfo01")]
+[assembly: AssemblyCopyright("Copyright © Tropical Designs 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05501ddd-5801-49b7-b0dc-d373851beab8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,7 @@
+## Hardware Info Tool
+
+## Description
+Application leverages Windows Management Instrumentation libraries to display client System Information
+
+## Notes
+Built in C# using Visual Studio 2008.  Works on XP only.
